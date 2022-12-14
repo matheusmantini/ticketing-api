b3cf3b78487de3b59ddf786e0c74320b1419149b
matheusmantini <engmatheusmantini@gmail.com>
2022-12-14 15:17:17 -0300

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!!!!!");
+});
+
+start();
